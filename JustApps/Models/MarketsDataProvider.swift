0ef54f5559beb360bbac308fec37037d4b7e8e84
dandreikiv <info@dma-mobile.dev>
2025-04-21 17:41:24 +0200

    Specify throwing error type

--- JustApps/Models/MarketsDataProvider.swift
+++ JustApps/Models/MarketsDataProvider.swift
@@ -1,25 +1,24 @@
 import Foundation
 
-enum DataLoaderError: Error {
+enum DataProviderError: Error {
     case invalidURL
     case decodingFailed
     case unknown
 }
 
 actor MarketsDataProvider: DataProvider {
-    func fetchData<T: Codable>() async throws -> T {
-        throw DataLoaderError.unknown
+    func fetchData<T: Codable>() async throws(DataProviderError) -> T {
         guard let url = URL(string: "https://jp-fallback.s3.us-east-2.amazonaws.com/fallback.json") else {
-            throw DataLoaderError.invalidURL
+            throw .invalidURL
         }
 
         do {
             let (data, _) = try await URLSession.shared.data(from: url)
             return try JSONDecoder().decode(T.self, from: data)
         } catch _ as DecodingError {
-            throw DataLoaderError.decodingFailed
+            throw .decodingFailed
         } catch {
-            throw DataLoaderError.unknown
+            throw .unknown
         }
     }
 }
